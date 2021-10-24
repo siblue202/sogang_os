e8216d80fdfc32eb883af4418f880814f52fede1
gyeonghwanjung <siblue202@gmail.com>
2021-10-24 21:46:23 +0900

    implementation user memory access (check_addr) using is_user_vadder

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,10 +1,12 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
-#include <syscall-nr.h>
+#include <syscall-nr.h>   // syscall-number 
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h" // for checking invalid address
 
 static void syscall_handler (struct intr_frame *);
+static void chech_addr(void *addr);
 
 void
 syscall_init (void) 
@@ -12,9 +14,19 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+static void 
+check_addr(void *addr){
+  if (!is_user_vaddr(addr)){
+    exit(-1);
+  }
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  // using switch case 
   printf ("system call!\n");
   thread_exit ();
 }
+
+
