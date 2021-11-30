c24a4a40d526dfbe83d47e9ea4697913920ad044
Jung GyeongHwan <siblue202@gmail.com>
2021-11-30 15:57:24 +0900

    thread_set_priority() 에서 new priority를 init priority로 설정해줘야 했음... 이거 해결하니
    FAIL tests/threads/priority-aging
    + mlfqs 제외하고 올 pass !
    
    코드 다듬자

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -441,7 +441,7 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  thread_current ()->init_priority = new_priority;
   // JGH 
 
   thread_lock_refresh();
